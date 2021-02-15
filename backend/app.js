fcc4ee9a8eb5c57be8030a283aac10a7316a2922
Rochak Jain <rochak_r@ec.iitr.ac.in>
2021-02-16 00:00:47 +0530

    Implement routeand controller for products

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+//import all the routes
+const products = require('./routes/product')
+
+app.get('/hw', function(req, res){
+    res.send('Hello World!')
+})
+
+app.use('/api/v1',  products)
+
 module.exports = app;
